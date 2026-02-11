7e7274ca46715659cc17dc9206b45061fb54dda5
Alfath Muqoddas <alfath.muqoddas@gmail.com>
2026-02-11 13:55:08 +0700

    update: add nuqs

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { NuqsAdapter } from "nuqs/adapters/react-router/v7";
 import { BrowserRouter } from "react-router";
 import "./index.css";
 
@@ -10,9 +11,11 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <NuqsAdapter>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </NuqsAdapter>
     </QueryClientProvider>
   </StrictMode>,
 );
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,3 +1,21 @@
+import { useQueryState } from "nuqs";
+
 export default function Search() {
-  return <h1 className="text-2xl font-bold">This is search page</h1>;
+  const [name, setName] = useQueryState("name");
+
+  const handleSubmit = (formData: any) => {
+    const name = formData.get("name");
+    setName(name);
+  };
+
+  return (
+    <>
+      <form action={handleSubmit}>
+        <input value={name || ""} onChange={(e) => setName(e.target.value)} />
+        <button type="submit">Submit</button>
+      </form>
+      <button onClick={() => setName(null)}>Clear</button>
+      <p>Hello, {name || "anonymous visitor"}!</p>
+    </>
+  );
 }

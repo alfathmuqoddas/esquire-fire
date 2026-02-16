1d86ab018936c7fd332ac5e18907f71c1482772a
Alfath Muqoddas <alfath.muqoddas@gmail.com>
2026-02-16 23:02:59 +0700

    update: update

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,18 @@
+import { addressOptions } from "../../config/options";
+
 export default function Home() {
-  return <h1 className="text-2xl font-bold">This is home page</h1>;
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <div className="p-4 border rounded-2xl w-full md:w-1/4 mx-8">
+          <h1 className="font-bold text-2xl">Search</h1>
+          <div className="fgrid grid-flow-col grid-cols-1 md:grid-cols-4">
+            <select name="provinsi" id="provinsi" className="col-span-1">
+              <option></option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
